ca99815769c9d3b493dad914fd93d314eeb8b08d
liuxiaodan <dbuyer@liuxiaodan.local>
2014-04-09 11:55:38 +0800

    Initial Commit

---
+++ DesignForZXL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DesignForZXL
+//
+//  Created by liuxiaodan on 14-4-9.
+//  Copyright (c) 2014年 liuxiaodan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DesignForZXL/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DesignForZXL
+//
+//  Created by liuxiaodan on 14-4-9.
+//  Copyright (c) 2014年 liuxiaodan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
